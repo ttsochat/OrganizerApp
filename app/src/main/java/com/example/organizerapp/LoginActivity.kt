31dd159991d97b94e009bdb0c148245c588be7e2
castawaypirate <apollonas@csd.auth.gr>
2021-04-21 22:03:42 +0300

    deleted temporary activity

--- app/src/main/java/com/example/organizerapp/LoginActivity.kt
+++ app/src/main/java/com/example/organizerapp/LoginActivity.kt
@@ -1,11 +1,38 @@
 package com.example.organizerapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+        val register = findViewById<Button>(R.id.register)
+        register.setOnClickListener{
+            startActivity(Intent(this,RegisterActivity::class.java))
+            finish()
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            updateUI(currentUser);
+        }
+    }
+
+    fun updateUI(currentUser: FirebaseUser?){
+
     }
 }
--- app/src/main/java/com/example/organizerapp/RegisterActivity.kt
+++ app/src/main/java/com/example/organizerapp/RegisterActivity.kt
@@ -2,10 +2,19 @@ package com.example.organizerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class RegisterActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
+
+
     }
+
+
 }
