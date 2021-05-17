8741475417abe830b103275b036d2199f3339d87
castawaypirate <apollonas@csd.auth.gr>
2021-05-17 20:01:57 +0300

    login done

--- app/src/main/java/com/example/organizerapp/LoginActivity.kt
+++ app/src/main/java/com/example/organizerapp/LoginActivity.kt
@@ -3,35 +3,82 @@ package com.example.organizerapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import com.example.organizerapp.databinding.ActivityMainBinding
+import com.example.organizerapp.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         auth = FirebaseAuth.getInstance()
 
-        val register = findViewById<Button>(R.id.register)
-        register.setOnClickListener{
+        binding.register.setOnClickListener{
             startActivity(Intent(this,RegisterActivity::class.java))
             finish()
         }
+
+        binding.login.setOnClickListener{
+            doLogin()
+        }
+    }
+
+    fun showToast(toast: String?) {
+        runOnUiThread {
+            Toast.makeText(this@LoginActivity, toast, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun doLogin() {
+        if(binding.email.text.toString().isEmpty()){
+            showToast("Please enter Email")
+            binding.email.requestFocus()
+            return
+        }
+        if(binding.password.text.toString().isEmpty()){
+            showToast("Please enter password")
+            binding.password.requestFocus()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(binding.email.text.toString(), binding.password.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    updateUI(null)
+                }
+            }
     }
 
     public override fun onStart() {
         super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
         if(currentUser != null){
             updateUI(currentUser);
         }
     }
 
-    fun updateUI(currentUser: FirebaseUser?){
+    private fun updateUI(currentUser: FirebaseUser?){
+        if(currentUser != null){
+            if(currentUser.isEmailVerified){
+                startActivity(Intent(this,NavigationActivity::class.java))
+                finish()
+            }else{
+                showToast("Please verify email address.")
+            }
+        }else{
+            showToast("Sign in failed.")
+        }
     }
 }
--- app/src/main/java/com/example/organizerapp/RegisterActivity.kt
+++ app/src/main/java/com/example/organizerapp/RegisterActivity.kt
@@ -1,18 +1,18 @@
 package com.example.organizerapp
 
+import android.R
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.organizerapp.databinding.ActivityRegisterBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthException
-import android.util.Log
+import com.google.firebase.auth.*
 
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-
     private lateinit var binding: ActivityRegisterBinding
     private val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
 
@@ -21,54 +21,60 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
         auth = FirebaseAuth.getInstance()
-
         binding.register.setOnClickListener{
             signUpUser()
         }
     }
 
+    fun showToast(toast: String?) {
+        runOnUiThread {
+            Toast.makeText(this@RegisterActivity, toast, Toast.LENGTH_SHORT).show()
+        }
+    }
+
     private fun signUpUser() {
         if(binding.username.text.toString().isEmpty()){
-            binding.username.error = "Please enter username"
+            showToast("Please enter username")
             binding.username.requestFocus()
             return
         }
-
         if(!binding.email.text.toString().trim().matches(emailPattern.toRegex())){
-            binding.email.error = "Invalid email"
+            showToast("Invalid email")
             binding.email.requestFocus()
             return
         }
-
         if(binding.password.text.toString().isEmpty()){
-            binding.password.error = "Please enter password"
+            showToast("Please enter password")
             binding.password.requestFocus()
             return
         }
-
         if(!binding.confirm.text.toString().equals(binding.password.text.toString())){
-            binding.confirm.error = "Password doesn't match"
+            showToast("Password doesn't match")
             binding.confirm.requestFocus()
             return
         }
-        auth.createUserWithEmailAndPassword(binding.email.text.toString(), binding.password.text.toString())
-                .addOnCompleteListener(this) { task ->
-                    Log.d("TAG", "Message1")
-                    if (task.isSuccessful) {
-                        Log.d("TAG", "Message2")
-                        startActivity(Intent(this, LoginActivity::class.java))
-                        finish()
-                    } else {
-                        Log.d("TAG", "Message3")
-                        val errorCode = (task.exception as FirebaseAuthException?)!!.errorCode
-                        Log.d("TAG", errorCode)
-//                        Toast.makeText(baseContext, "Sign up failed.",
-//                                Toast.LENGTH_SHORT).show()
+        auth.createUserWithEmailAndPassword(
+            binding.email.text.toString(),
+            binding.password.text.toString()
+        ).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                val user : FirebaseUser = auth.currentUser
+                user!!.sendEmailVerification()
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            startActivity(Intent(this, LoginActivity::class.java))
+                            finish()
+                        }
                     }
+            } else {
+                try {
+                    throw task.exception!!
+                } catch (e: Exception) {
+                    showToast(e.message!!)
+                    Log.e("TAG", e.message!!)
                 }
+            }
+        }
     }
-
-
 }
