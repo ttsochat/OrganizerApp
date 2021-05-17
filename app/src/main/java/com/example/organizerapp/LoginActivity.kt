556f5c3856c3c268a9b0adca051ef4287327d7f3
castawaypirate <apollonas@csd.auth.gr>
2021-05-17 21:40:34 +0300

    reset password ready

--- app/src/main/java/com/example/organizerapp/LoginActivity.kt
+++ app/src/main/java/com/example/organizerapp/LoginActivity.kt
@@ -1,11 +1,14 @@
 package com.example.organizerapp
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.organizerapp.databinding.ActivityMainBinding
 import com.example.organizerapp.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -14,6 +17,7 @@ import com.google.firebase.auth.FirebaseUser
 class LoginActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityMainBinding
+    private val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +34,36 @@ class LoginActivity : AppCompatActivity() {
         binding.login.setOnClickListener{
             doLogin()
         }
+
+        binding.forgot.setOnClickListener{
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Forgot password")
+            val view = layoutInflater.inflate(R.layout.dialog_forgot_password, null)
+            val fp_email = view.findViewById<EditText>(R.id.fp_email)
+            builder.setView(view)
+            builder.setPositiveButton("RESET", DialogInterface.OnClickListener{_, _->
+                forgotPassword(fp_email)
+            })
+            builder.setNegativeButton("CLOSE", DialogInterface.OnClickListener{_, _->
+            })
+            builder.show()
+        }
+    }
+
+    private fun forgotPassword(fp_email: EditText?) {
+        if(!fp_email?.text.toString().trim().matches(emailPattern.toRegex())){
+            return
+        }
+        if(fp_email?.text.toString().isEmpty()){
+            return
+        }
+
+        auth.sendPasswordResetEmail(fp_email?.text.toString())
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    showToast("Email sent.")
+                }
+            }
     }
 
     fun showToast(toast: String?) {
@@ -54,9 +88,9 @@ class LoginActivity : AppCompatActivity() {
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     val user = auth.currentUser
-                    updateUI(user)
+                    updateUI(user, true)
                 } else {
-                    updateUI(null)
+                    updateUI(null, true)
                 }
             }
     }
@@ -65,16 +99,16 @@ class LoginActivity : AppCompatActivity() {
         super.onStart()
         val currentUser = auth.currentUser
         if(currentUser != null){
-            updateUI(currentUser);
+            updateUI(currentUser, false);
         }
     }
 
-    private fun updateUI(currentUser: FirebaseUser?){
+    private fun updateUI(currentUser: FirebaseUser?, login: Boolean){
         if(currentUser != null){
             if(currentUser.isEmailVerified){
                 startActivity(Intent(this,NavigationActivity::class.java))
                 finish()
-            }else{
+            }else if(login){
                 showToast("Please verify email address.")
             }
         }else{
