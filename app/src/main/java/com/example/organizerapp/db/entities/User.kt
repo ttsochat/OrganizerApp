333b200ba833d50ed00e02a47968199dc765c1b7
mariapaikou <ppppaikoy999@gmail.com>
2021-05-30 18:49:27 +0300

    comments for User.kt ready!

--- app/src/main/java/com/example/organizerapp/db/entities/User.kt
+++ app/src/main/java/com/example/organizerapp/db/entities/User.kt
@@ -4,6 +4,9 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+/**
+ Data class User hold the data of a user object.
+ */
 @Entity(tableName = "user_table")
 data class User(
     @PrimaryKey val uid: String,
