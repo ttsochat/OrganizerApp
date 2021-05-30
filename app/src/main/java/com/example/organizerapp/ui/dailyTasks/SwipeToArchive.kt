12c86e889a3f879b00e726fe1e40cc16ab4df4df
ttsochat <ttsochat@csd.auth.gr>
2021-05-30 19:43:51 +0300

    Swipes comments Details: rotation off in login & register app icon added padding to the card

--- app/src/main/java/com/example/organizerapp/ui/dailyTasks/SwipeToArchive.kt
+++ app/src/main/java/com/example/organizerapp/ui/dailyTasks/SwipeToArchive.kt
@@ -1,16 +1,15 @@
 package com.example.organizerapp.ui.dailyTasks
 
-import android.graphics.Canvas
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 
+/**
+ * Action class for when the user swipes the cards to the left
+ */
 abstract class SwipeToArchive: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
 
     override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
         return false
     }
 
-    override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
-        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
-    }
 }
--- app/src/main/java/com/example/organizerapp/ui/dailyTasks/SwipeToDone.kt
+++ app/src/main/java/com/example/organizerapp/ui/dailyTasks/SwipeToDone.kt
@@ -1,25 +1,15 @@
 package com.example.organizerapp.ui.dailyTasks
 
-import android.content.Context
-import android.graphics.Canvas
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 
+/**
+ * Action class for when the user swipes the cards to the right
+ */
 abstract class SwipeToDone :ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
 
     override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
         return false
     }
 
-    override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
-        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
-//        var builder : RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
-//        .addBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.my_background))
-//                .addActionIcon(R.drawable.my_icon)
-//                .create()
-//                .decorate();
-//        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
-//        RecyclerViewSwipeDecoratorBuilder
-    }
 }
