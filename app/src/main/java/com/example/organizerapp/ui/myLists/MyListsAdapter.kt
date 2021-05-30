003370d99045e5f7e688af784dc8bf43e3b59357
mariapaikou <ppppaikoy999@gmail.com>
2021-05-30 18:35:41 +0300

    comments for MyListAdapter.kt ready!

--- app/src/main/java/com/example/organizerapp/ui/myLists/MyListsAdapter.kt
+++ app/src/main/java/com/example/organizerapp/ui/myLists/MyListsAdapter.kt
@@ -10,24 +10,39 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.organizerapp.R
 import com.example.organizerapp.db.entities.MyList
 
-
+/**
+  MyListsAdapter class extents from ListAdapter and is responsible for converting data from the
+  database to cardViews in order to display them on screen. It also takes as parameter a function
+  given to the adapter via MyListFragment to add on click functionality to the card views.
+ */
 class MyListsAdapter (private val onClick: (MyList) -> Unit):
     ListAdapter<MyList, MyListsAdapter.ListsViewHolder>(ListsDiffCallBack){
 
-
+    /**
+        Overwritten function onCreateViewHolder inflates my_list_item layout to the adapter to display
+        the data according to the cardViews design.
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListsViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.my_list_item, parent,false)
         return ListsViewHolder(view, onClick)
     }
 
+    /**
+        Overwritten function onBindViewHolder connects new cardView crated by the ListViewHolder class
+        to the recycler view of MyListFragment.
+     */
     override fun onBindViewHolder(holder: ListsViewHolder, position: Int) {
         val currentCardView = getItem(position)
         holder.connect(currentCardView)
 
     }
 
-
-    //View Holder
+    /**
+        This class is the ViewHolder of the MyListsAdapter class.
+        ListsViewHolder class extents from RecyclerView ViewHolder and View.OnCreateContextMenuListener
+        in order to create every card View about to be displayed on screen and add to them a popup
+        menu to give them extra functionalities such as Delete Item by by clicking popup menu option.
+     */
     class ListsViewHolder(cardView : View, val onClick: (MyList) -> Unit) : RecyclerView.ViewHolder(cardView) ,
         View.OnCreateContextMenuListener {
 
@@ -36,7 +51,10 @@ class MyListsAdapter (private val onClick: (MyList) -> Unit):
         val  layout: LinearLayout = cardView.findViewById(R.id.my_list_item_layout)
         private var currentList: MyList? = null
 
-        //click listener for card view
+        /**
+            Initializing block for adding on click listener to card view and onCreateContextMenuListener
+            on every new cardView.
+         */
         init {
             cardView.setOnCreateContextMenuListener(this)
             cardView.setOnClickListener {
@@ -46,6 +64,10 @@ class MyListsAdapter (private val onClick: (MyList) -> Unit):
             }
         }
 
+        /**
+            Function connect takes as parameter the list about to be displayed on screen and sets it
+            title and list to the correct text Views of the cardView.
+         */
         fun connect(list : MyList){
             currentList = list
             title.text = list.title
@@ -53,7 +75,11 @@ class MyListsAdapter (private val onClick: (MyList) -> Unit):
         }
 
 
-        //attach context menu
+        /**
+            Overwritten function onCreateContextMenu attaches a popup menu to every cardView with one
+            option. Option is Delete List and it is used when user wants to permanently delete a list
+            from the database.
+         */
         override fun onCreateContextMenu(
             menu: ContextMenu?,
             v: View?,
@@ -70,7 +96,10 @@ class MyListsAdapter (private val onClick: (MyList) -> Unit):
 
 }
 
-
+/**
+    Object ListsDiffCallBack extents DiffUtil.ItemCallback and is used for updating a single item
+    when this item is changes instead of updating the whole list of items displayed on screen.
+ */
 object ListsDiffCallBack: DiffUtil.ItemCallback<MyList>(){
     override fun areItemsTheSame(oldItem: MyList, newItem: MyList): Boolean {
         return oldItem == newItem
